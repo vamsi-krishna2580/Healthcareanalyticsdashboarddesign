d102c94dc800425488f7ce41f4a78850e5f1f974
vamsi-krishna2580 <luckky45907@gmail.com>
2026-01-31 17:28:33 +0530

    31-01-2026 17:28

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,15 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: './',   // 👈 add this line
+
   plugins: [
     // The React and Tailwind plugins are both required for Make, even if
     // Tailwind is not being actively used – do not remove them
     react(),
     tailwindcss(),
   ],
+
   resolve: {
     alias: {
       // Alias @ to the src directory
